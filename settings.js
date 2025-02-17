4970182ac8cd95b3eed5ba3d679767b9cc3c813a
pazbotzy <ptjasteb170@gmail.com>
2025-02-17 19:55:58 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,14 @@
+const settings = {
+  token: '7497353146:AAGpv3_gsqpcCyk5_4GfIrqb7UN1V5Mr5GU', // Token Bot
+  adminId: '5849385750',
+  urladmin: 'https://t.me/Pazbotz',
+  domain: 'https://pribadi.sevs.my.id', // domain
+  plta: 'ptla_i0eF8OVDQKzJt9Z2cGNMNly7oxeD6ZsA1mXfMtkpdH1', // plta yang sesuai
+  pltc: 'ptlc_8mQi2wCd2AgjZLCoMcN1i2lfXOOmGSECXUZJ5vE8sOH', // pltc yang sesuai
+  
+  //CREATE PANEL
+  loc: '1', // Isi dengan lokasi yang diinginkan
+  eggs: '15'
+};
+
+module.exports = settings;
